6a97b4b155000cc82f7a73cf6719ad86e2b0c895
Eugene Kravec <krava255@gmail.com>
2021-10-12 11:33:12 +0300

    Second solution - Wrong Answer

--- tasks/twoSum.js
+++ tasks/twoSum.js
@@ -17,17 +17,43 @@
 // Input: nums = [3, 3], target = 6
 // Output: [0, 1]
 
+//================================================
 // Firest solution  - Example 2 - testing negative
-// But LeetCode - done Accepted Runtime: 64 ms
+
+// var twoSum = function (nums, target) {
+//     let sum = 0;
+//     let arr = []
+//     for (let i = 0; i < nums.length; i++) {
+//         sum += nums[i]
+//         if (sum === target) {
+//             arr.push(i - 1)
+//             arr.push(i)
+//         }
+//     }
+
+//     return arr
+// };
+// console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 2, 4], 6));
+// console.log(twoSum([3, 3], 6));
+
+//================================================
+
+//================================================
+// Second solution - Wrong Answer
 
 var twoSum = function (nums, target) {
-    let sum = 0;
-    let arr = []
+    const arr = []
     for (let i = 0; i < nums.length; i++) {
-        sum += nums[i]
-        if (sum === target) {
-            arr.push(i - 1)
-            arr.push(i)
+
+        for (let y = 1; y < nums.length; y++) {
+
+            if ((nums[i] + nums[y]) === target) {
+                arr.push(i)
+                arr.push(y)
+                return arr
+            }
+
         }
     }
 
